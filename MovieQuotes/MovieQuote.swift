48ee2f47b54a84dd86a175ad0333b6e97f2c6f41
rhit-yangh6 <yangh6@rose-hulman.edu>
2021-01-11 16:05:36 +0800

    Convert my table to show movie quotes

---
+++ MovieQuotes/MovieQuote.swift
@@ -0,0 +1,18 @@
+//
+//  MovieQuote.swift
+//  MovieQuotes
+//
+//  Created by Hanyu Yang on 2021/1/11.
+//
+
+import Foundation
+
+class MovieQuote {
+    var quote: String
+    var movie: String
+    
+    init(quote: String, movie: String) {
+        self.quote = quote
+        self.movie = movie
+    }
+}
