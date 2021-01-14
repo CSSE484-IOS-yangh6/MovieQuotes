67e5e3678a1640a33b9cee2a09db7c02d68b2822
rhit-yangh6 <yangh6@rose-hulman.edu>
2021-01-15 02:24:11 +0800

    UI for email password

---
+++ MovieQuotes/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  MovieQuotes
+//
+//  Created by Hanyu Yang on 2021/1/15.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    let showListSegueIdentifier = "ShowListSegue"
+    
+    @IBAction func pressedSignInNewUser(_ sender: Any) {
+    }
+    
+    @IBAction func pressedLogInExistingUser(_ sender: Any) {
+    }
+}
